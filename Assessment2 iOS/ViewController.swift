09b93ceb86ca724c88584532c4e3dbb6f4a360f6
BSASEF01 <bersabehsolomon16@gmail.com>
2018-04-18 15:59:11 -0400

    complet Assessementios2

--- Assessment2 iOS/ViewController.swift
+++ Assessment2 iOS/ViewController.swift
@@ -9,16 +9,63 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    class mockup {
+        var fristName: String
+        var lastName: String
+        var email: String
+        
+        init(fristName: String, lastName: String, email: String) {
+            self.fristName = fristName
+            self.lastName = lastName
+            self.email = email
+        }
+        
+    }
+    
+    
+    @IBOutlet weak var fristName: UITextField!
+    
+    @IBOutlet weak var lastName: UITextField!
+    
+    @IBOutlet weak var Email: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
+
+    @IBAction func Button1(_ sender: Any) {
+        
+        var person = mockup(fristName:  fristName.text! , lastName: lastName.text!, email: Email.text!)
+     
+        if fristName.text == "" || lastName.text == "" || Email.text == "" {
+            let mockup = (fristname: fristName.text!,lastname : lastName.text!)
+        
+        let    alertController = UIAlertController(title: "Error", message: "You need to fill out all text boxes.", preferredStyle: .alert)
+    let closeAction = UIAlertAction(title: "Close", style: .default, handler: nil)
+    alertController.addAction(closeAction)
+    self.present(alertController, animated: true, completion: nil)
+    
+    } else {
+    
+            let alertViewController = UIAlertController(title: "Great", message: "Hello \(person.fristName) \(person.lastName) Your email address is \(person.email)", preferredStyle: .alert)
+    let closeAction = UIAlertAction(title: "Okey", style: .default, handler: nil)
+    alertViewController.addAction(closeAction)
+    self.present(alertViewController, animated: true, completion: nil)
+    }
+    
+    
+    
+    
+    
+    }
+    
 
 
 }
